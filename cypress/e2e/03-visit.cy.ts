ffac75d875ccf5fa2a7693dcc5cf6d1264eddcc6
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-30 20:10:07 -0300

    .then

--- cypress/e2e/03-visit.cy.ts
+++ cypress/e2e/03-visit.cy.ts
@@ -1,12 +1,15 @@
 // Get or visit a website using Cypress
 // con cy.visit() y pasandole un string o un objeto con funciones puede entrar a una pagina
 
-describe('basics', () => {  
-    it('visit textinput', () => {
-      cy.visit('/textinput')
+describe('basics', () => {
+    // antes de cada test entra a la pagina del beforeEach
+    beforeEach(() => {
+        cy.visit('/textinput')
     })
+    // algunos metodos son yell solo llamar y obtiene un valor que luego puede ser usado mediante  .then() como en las promesas
     it('visit classattr', () => {
-      cy.visit('/classattr')
+        cy.url().then((url) => {
+            cy.log(`Printing the URL ${url}`)
+        })
     })
-  })
-  
+})
