c8e3cd47c7ac6294537a70b05969a11d0b94fa95
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-30 15:33:26 -0300

    add to new repo

---
+++ cypress/e2e/helloworld.cy.ts
@@ -0,0 +1,12 @@
+
+
+
+describe('template spec', () => {  
+  it('passes', () => {
+    cy.log('hello world')
+  })
+})
+
+
+// Usa Mocha para los test, para llamar a una funcion tenemos que poner CY. y ls opciones
+// el describe nos indica que es un nuevo test y adentro eta formanado por un string y una funcinon
---
+++ cypress/e2e/mocha-demo.cy.ts
@@ -0,0 +1,13 @@
+// con los IT se pueden hacer multiples test bajo una misma prueba por cada It sera una parte del todo
+
+describe('Pruebas en Mocha', ()=>{
+    it('Testcase #1',()=>{
+        cy.log('Hello World');
+    });
+    it('Testcase #2',()=>{
+        cy.log('Hello World');
+    });
+    it('Testcase #3',()=>{
+        cy.log('Hello World');
+    });
+})
