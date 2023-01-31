24d2298bb7a35bb8b19f00f159e8be28def425cb
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-01-31 16:02:14 -0300

    xpath with spaces

--- cypress/e2e/05-xpath-selectors.cy.ts
+++ cypress/e2e/05-xpath-selectors.cy.ts
@@ -1,8 +1,14 @@
-describe('Locators',()=>{
-    beforeEach(()=>{
+describe('Locators', () => {
+    beforeEach(() => {
         cy.visit('/classattr');
     });
-    it('How to find an element by its text',()=>{
-        cy.xpath(`//*[text()='Correct variant is']`).should('contain.text','Correct')
+    it('How to find an element by its text', () => {
+        cy.xpath(`//*[text()='Correct variant is']`).should('contain.text', 'Correct')
+    });
+    it('Find an element by its attribute using xpath', () => {
+        cy.xpath(`//pre[@class=' language-html']`).should('contain.text', 'button');
+    });
+    it('Find by class (middle and spaces)', () => {
+        cy.xpath(`//button[contains(concat(' ',@class,' '),'btn-warning')]`).should('have.css', 'background-color', 'rgb(255, 193, 7)')
     })
 })
