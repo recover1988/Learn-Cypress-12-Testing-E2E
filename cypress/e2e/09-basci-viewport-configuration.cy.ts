e6dddcd0f1b2e9cbbf11a3aa4a138fbfd49f4500
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-01 13:41:47 -0300

    change name

---
+++ cypress/e2e/09-basci-viewport-configuration.cy.ts
@@ -0,0 +1,7 @@
+describe('cy.viewport() demo', () => { 
+    beforeEach(()=>{
+        cy.visit(`${Cypress.env('Angular')}/angularjs-protractor-practice-site`)
+    });
+    it('device name',()=>{});
+    it('specific viewport',()=>{});
+ })
