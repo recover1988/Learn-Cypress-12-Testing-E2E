3d2425469672ccd981214b94859a43026a78f537
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-01 14:53:41 -0300

    links

---
+++ cypress/e2e/12-links.cy.ts
@@ -0,0 +1,8 @@
+describe('Dealing with links', ()=>{
+    beforeEach(()=>{
+        cy.visit(`${Cypress.env('demoQA')}/links`)
+    });
+    it('First approach, not click on the link', ()=>{
+        
+    })
+})
