a6b9d5e94d07268f21d5c8f4514f5d3f5a3ff1bf
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-01 14:56:21 -0300

    change name

--- cypress/e2e/12-links.cy.ts
+++ cypress/e2e/12-links.cy.ts
@@ -1,8 +1,9 @@
-describe('Dealing with links', ()=>{
-    beforeEach(()=>{
+describe('Dealing with links', () => {
+    beforeEach(() => {
         cy.visit(`${Cypress.env('demoQA')}/links`)
     });
-    it('First approach, not click on the link', ()=>{
-        
+    it('First approach, not click on the link', () => {
+        cy.get('#simplelink').should('have.attr', 'href', 'https://demoqa.com/')
+        cy.get('#simplelink').should('have.attr', 'target', '_blank')
     })
 })
