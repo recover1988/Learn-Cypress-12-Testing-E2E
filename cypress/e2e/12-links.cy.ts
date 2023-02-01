01560c92041c9dd64177c5da6a7e3e20a159abbf
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-01 15:20:26 -0300

    intercept api call

--- cypress/e2e/12-links.cy.ts
+++ cypress/e2e/12-links.cy.ts
@@ -2,8 +2,30 @@ describe('Dealing with links', () => {
     beforeEach(() => {
         cy.visit(`${Cypress.env('demoQA')}/links`)
     });
+    it.skip('First approach, not click on the link', () => {
+        cy.get('#simpleLink').should('have.attr', 'href', 'https://demoqa.com')
+        cy.get('#simpleLink').should('have.attr', 'target', '_blank')
+    });
+    it.skip('Second approach, remove the target', () => {
+        cy.get('#simpleLink').invoke('removeAttr', 'target').click();
+        cy.url().then((url) => {
+            expect(url).to.be.equal('https://demoqa.com/')
+        })
+    });
+})
+
+describe('Intercepting API(SPYING) request after clicking on a button', () => {
+    beforeEach(() => {
+        cy.visit(`${Cypress.env('demoQA')}/links`);
+        cy.intercept('GET', `${Cypress.env('demoQA')}/created`).as('linkStatus')
+    });
     it('First approach, not click on the link', () => {
-        cy.get('#simplelink').should('have.attr', 'href', 'https://demoqa.com/')
-        cy.get('#simplelink').should('have.attr', 'target', '_blank')
-    })
+        cy.get('a#created').click();
+        cy.wait('@linkStatus').then((request) => {
+            cy.log(`This is the request intercepted ${request}`);
+            expect(request.response.statusCode).to.be.equal(201)
+            expect(request.response.statusMessage).to.be.equal('Created')
+        })
+    });
+
 })
