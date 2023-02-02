bcf13929bde99f8cabfb145318f8b0e9cbb12e68
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-02 14:16:32 -0300

    solve type error

--- cypress/e2e/13-broken-images.cy.ts
+++ cypress/e2e/13-broken-images.cy.ts
@@ -1,3 +1,6 @@
+
+let img: HTMLImageElement;
+
 describe('Broken images with Demo QA', () => {
     beforeEach(() => {
         cy.visit(`${Cypress.env('demoQA')}/broken`);
@@ -6,15 +9,16 @@ describe('Broken images with Demo QA', () => {
         cy.get(`div > img[src="/images/Toolsqa.jpg"]`)
             .should('be.visible')
             .and(($img) => {
-                cy.log(`This is the img yielded`, $img)
+                img = $img[0] as unknown as HTMLImageElement
+                expect(img.naturalWidth).to.be.greaterThan(0)
             });
     });
     it('Broken Image Assertion', () => {
         cy.get(`div > img[src="/images/Toolsqa_1.jpg"]`)
             .should('be.visible')
             .and(($img) => {
-                cy.log(`This is the img yielded`, $img)
-                expect($img[0].naturalWidth).to.be.greaterThan(0)
+                img = $img[0] as unknown as HTMLImageElement
+                expect(img.naturalWidth).to.be.greaterThan(0)
             });
     })
 })
