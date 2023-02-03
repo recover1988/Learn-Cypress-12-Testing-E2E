278ec12f6f1a04ddaf8e569489e78737cfad5751
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-03 15:00:17 -0300

    use type in fixture,then

--- cypress/e2e/28-fixtures-basics.cy.ts
+++ cypress/e2e/28-fixtures-basics.cy.ts
@@ -1,6 +1,7 @@
 import { testData } from "./model";
 describe("Using a fixture", () => {
   it("Loading the file and display the data in the log", () => {
+    // .fixture --> busca en la carpeta fixtures y solo tenemos q poner le nombre del archivo
     cy.fixture("jsonExample").then((testData: testData) => {
       cy.log("Printing objects", testData);
       cy.log("Printing objects", testData.property1);
