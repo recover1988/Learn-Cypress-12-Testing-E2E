a5df9c20b23e418f570799c22fc8083d08f97077
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-03 14:41:32 -0300

    create new command en commands.ts

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -39,3 +39,29 @@ import '@4tw/cypress-drag-drop';
 //     }
 //   }
 // }
+//
+declare global {
+    namespace Cypress {
+      interface Chainable {
+        login(email: string, password: string): Chainable<void>;
+        safeLogin(email: string, password: string): Chainable<void>;
+        parseXlsx(inputFile: any): any;
+      }
+    }
+  }
+  
+  Cypress.Commands.add("login", (username: string, password: string) => {
+    cy.get("#userName").type(username);
+    cy.get("#password").type(password);
+    cy.get("#login").click();
+  });
+  
+  Cypress.Commands.add("safeLogin", (username: string, password: string) => {
+    cy.get("#userName").type(username);
+    cy.get("#password").type(password,{log: false});
+    cy.get("#login").click();
+  });
+  
+  Cypress.Commands.add("parseXlsx", (inputFile) => {
+    return cy.task("parseXlsx", { filePath: inputFile });
+  });
